64feff3dec92b84c08e8755714d4c40c7fe7d72d
ridham181 <78419223+ridham181@users.noreply.github.com>
2022-06-22 20:36:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function take_snapshot(){
+save('my_image.png');
+
+}
+function preload(){
+
+}
+
+function draw(){
+
+}
